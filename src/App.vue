137a901550910ecaad213cafa893ad8242a1f9c3
djh <zhaoxiaqinghui@gmail.com>
2022-07-30 22:31:14 +0800

    头部编写

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
-<!--  router-view 展示路由对应的组件内容-->
-  <router-view/>
+    <!--  router-view 展示路由对应的组件内容-->
+    <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
-nav {
-  padding: 30px;
-}
+    nav {
+        padding: 30px;
+    }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    nav a.router-link-exact-active {
+        color: #42b983;
+    }
+
+    .flex-float {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .flex {
+        display: flex;
+        align-items: center;
+    }
 </style>
--- src/views/Layout/LayOut.vue
+++ src/views/Layout/LayOut.vue
@@ -1,9 +1,17 @@
 <template>
     <div class="common-layout">
         <el-container>
-            <el-header>Header</el-header>
+            <el-header class="common-header flex-float">
+                <div class="flex">
+                    <img class="logo" src="../../assets/logo.png" alt="">
+                    <h1 class="title">菜品后台管理系统</h1>
+
+                </div>
+                <el-button type="danger">退出</el-button>
+            </el-header>
+
             <el-container>
-                <el-aside width="200px">
+                <el-aside class="common-aside" width="200px">
                     <router-link to="/index">食物列表</router-link>
                 </el-aside>
                 <el-main>
@@ -22,6 +30,23 @@
     }
 </script>
 
-<style scoped>
-
+<style>
+    .el-container{
+        height:100vh;
+        /*溢出 隐藏*/
+        overflow: hidden;
+    }
+    .common-aside{
+        background: #2c3945;
+    }
+    .common-header {
+        background: #202932;
+        display: flex;
+    }
+    .logo{
+        width: 80px;
+    }
+    .title{
+        color: #ffffff;
+    }
 </style>
